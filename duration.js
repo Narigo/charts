0ce60955e6788a40900085603dc1980ad7f90253
Joern Bernhardt <joern.bernhardt@googlemail.com>
2018-06-08 22:06:38 +0200

    Start trying to fix duration (or get it out as key)
    Signed-off-by: Joern Bernhardt <joern.bernhardt@googlemail.com>

---
+++ duration.js
@@ -0,0 +1,40 @@
+const fs = require("fs");
+const { promisify } = require("util");
+
+return run().catch(e => console.error("Error!", e));
+
+async function run() {
+  const readdir = promisify(fs.readdir);
+  const readFile = promisify(fs.readFile);
+  const writeFile = promisify(fs.writeFile);
+  const resultsDir = `${__dirname}/results`;
+
+  const files = await readdir(resultsDir);
+  const result = await files.filter(name => /result-\d+-\d+\.json$/.test(name)).reduce(async (acc, jsonFile) => {
+    const resultJson = await acc;
+    const json = await readFile(`${resultsDir}/${jsonFile}`);
+    const fixedJson = fixDurations(JSON.parse(json));
+    return {
+      ...resultJson,
+      ...fixedJson
+    };
+  }, Promise.resolve({}));
+
+  await writeFile(`${resultsDir}/result.json`, JSON.stringify(result));
+}
+
+function fixDurations(json) {
+  return Object.entries(json).reduce((acc, [year, { singles, albums }]) => {
+    return {
+      ...acc,
+      [year]: {
+        singles: [...singles.map(single => ({ ...single, duration: fixDuration(single.duration) }))],
+        albums: [...albums.map(album => ({ ...album, duration: fixDuration(album.duration) }))]
+      }
+    };
+  }, {});
+
+  function fixDuration(duration) {
+    return "unfixed:" + duration;
+  }
+}
