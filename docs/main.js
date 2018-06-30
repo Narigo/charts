4ee213e21c756102641b1ee3068935e873e9704c
Joern Bernhardt <joern.bernhardt@googlemail.com>
2018-06-30 21:34:31 +0200

    Make single and album stylable
    Signed-off-by: Joern Bernhardt <joern.bernhardt@googlemail.com>

--- docs/main.js
+++ docs/main.js
@@ -18,12 +18,8 @@ function onChange(e) {
   const hitSingle = getHit(date, charts[year].singles);
   const hitAlbum = getHit(date, charts[year].albums);
 
-  const $singles = document.createElement("div");
-  $singles.appendChild(document.createTextNode(`Single: ${hitSingle.interpret} - ${hitSingle.title}, ${hitSingle.duration.from} bis ${hitSingle.duration.to}`));
-  $singles.classList.add("singles");
-  const $albums = document.createElement("div");
-  $albums.appendChild(document.createTextNode(`Album: ${hitAlbum.interpret} - ${hitAlbum.title}, ${hitAlbum.duration.from} bis ${hitAlbum.duration.to}`));
-  $albums.classList.add("albums");
+  const $singles = createSingleOrAlbum("Single", "single", hitSingle);
+  const $albums = createSingleOrAlbum("Album", "album", hitAlbum);
 
   $result.innerHTML = "";
   $result.appendChild($singles);
@@ -31,5 +27,36 @@ function onChange(e) {
 
   function getHit(date, listOfHits) {
     return listOfHits.find(hit => hit.duration.from <= date && date <= hit.duration.to);
+  }
+
+  function createSingleOrAlbum(name, singleOrAlbum, hit) {
+    const $singleOrAlbum = document.createElement("div");
+    const $interpret = document.createElement("span");
+    const $title = document.createElement("span");
+    const $durationFrom = document.createElement("span");
+    const $durationUntil = document.createElement("span");
+
+    $interpret.appendChild(document.createTextNode(hit.interpret));
+    $title.appendChild(document.createTextNode(hit.title));
+    $durationFrom.appendChild(document.createTextNode(hit.duration.from));
+    $durationUntil.appendChild(document.createTextNode(hit.duration.to));
+
+    $interpret.classList.add("interpret");
+    $title.classList.add("title");
+    $durationFrom.classList.add("durationFrom");
+    $durationUntil.classList.add("durationUntil");
+
+    $singleOrAlbum.appendChild(document.createTextNode(name));
+    $singleOrAlbum.appendChild(document.createTextNode(": "));
+    $singleOrAlbum.appendChild($interpret);
+    $singleOrAlbum.appendChild(document.createTextNode(" - "));
+    $singleOrAlbum.appendChild($title);
+    $singleOrAlbum.appendChild(document.createTextNode(", "));
+    $singleOrAlbum.appendChild($durationFrom);
+    $singleOrAlbum.appendChild(document.createTextNode(" bis "));
+    $singleOrAlbum.appendChild($durationUntil);
+    $singleOrAlbum.classList.add(singleOrAlbum);
+
+    return $singleOrAlbum;
   }
 }
